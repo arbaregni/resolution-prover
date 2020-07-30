165d5ac8c5257653ebb34717902a2642f772142d
arbaregni <jamesnsours@gmail.com>
2020-07-29 21:13:12 -0400

    - added impossible_pathing.rs

---
+++ examples/impossible_pathing.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let givens = vec![
+        "forall x: forall y: forall z: (Path(x,y) & Path(y,z)) => Path(x,z)",
+        "Path(a, b)",
+        "Path(b, c)",
+    ];
+    let goal = "Path(d, e)";
+    let result = resolution_prover::find_proof(givens.as_slice(), goal);
+    println!("result: {:?}", result);
+}
